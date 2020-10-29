ea16dbb8daa550a49de3b26252450e8474cc217e
Sean Noyes <sean@razor>
2020-10-29 18:37:06 -0500

    dev efforts

---
+++ scripts/delete-alerts.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+wf alert delete -t $1 -E longboard.wavefront.com 1602089154666
+wf alert delete -t $1 -E longboard.wavefront.com 1602089149772
+wf alert delete -t $1 -E longboard.wavefront.com 1602089473519
+wf alert delete -t $1 -E longboard.wavefront.com 1602089471916
+wf alert delete -t $1 -E longboard.wavefront.com 1602089147720
